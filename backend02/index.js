c52274bc5fbe5c9ba82da8bfae93ab6d07544613
govind07 <govindrajput02653@gmail.com>
2024-11-12 16:08:46 +0530

    fs

--- backend02/index.js
+++ backend02/index.js
@@ -1,14 +1,25 @@
 const http = require('http');
+const fs = require('fs');
+const url = require('url');
 
 
 
 // creating a  server
-http.createServer((req, resp )=>{
-    // console.log(req.url)
-    if(req.method === "PUT"){
-        console.log("this is not get method")
+http.createServer((req, res )=>{
+
+    let products = fs.readFileSync('./products.json','utf-8')
+
+    const path = url.parse(req.url,true)
+     
+    // res.end('nothing')
+    
+    if(path.pathname=='/products' && path.query.id == undefined && req.method=="GET"){
+       res.end(products)  
     }
-    // console.log(req.method)
-    resp.end("this is first response")
+    else if(path.pathname=='/products' && path.query.id !== undefined && req.method=="GET"){
+        const pro =  JSON.parse(products)
+        console.log(pro)
+    }
+   
   
 }).listen(8000)
