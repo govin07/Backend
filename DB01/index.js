bbf818d8c83786925900fa5b0ac36486637c4fa8
govind07 <govindrajput02653@gmail.com>
2024-12-24 08:30:31 +0530

    Mongoose

--- DB01/index.js
+++ DB01/index.js
@@ -11,3 +11,48 @@ mongoose.connect("mongodb://localhost:27017/apidev-demo")
 
 
 // schema
+
+const UserSchema = mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+    },
+    age:{
+        type:Number,
+        max: [100, "chl be itna kon jita he "],
+        min: [20, "goli beta masti nhi "],
+    },
+    email:String,
+})
+
+// model  
+const userModel = mongoose.model("users", UserSchema)
+
+// inserting data
+
+// let user = {
+//     name:"saching",
+//     age:15,
+//     email: "govindrajput02653@gmail.com"
+// }
+
+// userModel.create(user)
+// .then((res)=>{
+//     console.log("data inserted")
+
+// })
+// .catch((err)=>{
+//     console.log("data is not inserted");
+//     throw err
+
+// })
+
+userModel.find({name:"sachin"})
+.then((data)=>{
+    console.log(data)
+    
+})
+.catch((err)=>{
+    console.log(err)
+
+})
